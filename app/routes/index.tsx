de99f8b8e157ac74397cce82ddea79db6c2cc7fb
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-12-14 13:46:58 -0300

    feat(home): disable buttons when they cant be used

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,31 @@
-import { Form, Link } from "@remix-run/react"
+import type { LoaderArgs } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import { Form, Link, useLoaderData } from "@remix-run/react"
 import { PrimaryButton } from "~/components/primary-button"
+import { getUserAddress } from "~/utils/session.server"
+
+export async function loader({ request }: LoaderArgs) {
+  const userAddress = await getUserAddress(request)
+  const isLoggedIn = Boolean(userAddress)
+
+  return json({ isLoggedIn })
+}
 
 export default function Index() {
+  const { isLoggedIn } = useLoaderData<typeof loader>()
+
   return (
     <div className="flex h-full w-full items-center justify-center space-x-2">
       <Link to="login">
-        <PrimaryButton>Login</PrimaryButton>
+        <PrimaryButton disabled={isLoggedIn}>Login</PrimaryButton>
       </Link>
       <Link to="join">
-        <PrimaryButton>Join</PrimaryButton>
+        <PrimaryButton disabled={isLoggedIn}>Join</PrimaryButton>
       </Link>
       <Form action="/logout" method="post">
-        <PrimaryButton type="submit">Logout</PrimaryButton>
+        <PrimaryButton type="submit" disabled={!isLoggedIn}>
+          Logout
+        </PrimaryButton>
       </Form>
       <Link to="user">
         <PrimaryButton type="submit">User</PrimaryButton>
